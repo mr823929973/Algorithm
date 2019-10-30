69b1bbd13d52c987414d7b1d697142f42e03059b
mr823929973 <mr823929973@buaa.edu.cn>
2019-10-30 21:19:49 +0800

    10-30

---
+++ C3-F.cpp
@@ -0,0 +1,21 @@
+#include<algorithm>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+int dp[2001][2001][2];
+int main()
+{
+    int n,m,x,y,i,j;
+    while(scanf("%d%d%d%",&n,&m,&x,&y)!=EOF)
+    {
+        dp[0][0][0]=dp[0][0][1]=0;
+        for(i=1;i<=m;i++)
+        {
+            for(j=1;j<=i;i++)
+            {
+
+            }
+        }
+    }
+    return 0;
+}
