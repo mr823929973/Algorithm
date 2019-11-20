b6c5fa1fc36e506df230992db7d5c4f8367b9439
mr823929973 <mr823929973@buaa.edu.cn>
2019-11-20 19:46:24 +0800

    凸包

--- C5-H.cpp
+++ C5-H.cpp
@@ -10,10 +10,10 @@
 using namespace std;
 
 const double eps = 1e-8,pi=3.14159265;
-// 若 一点的坐标在前面两点的右边，则更新右端点 
+
 int n;
 
-int dcmp(double x)  // 三态函数 ，在下面用于是否在精度 
+int dcmp(double x)  
 {
 	if(fabs(x)<eps)return 0;
 	else return x>0?1:-1;
@@ -23,7 +23,7 @@ struct Point
 {
 	double x,y;
 	inline Point(double x=0,double y=0):x(x),y(y){}
-}p[10000+5],ch[10000+5];
+}p[100000+5],ch[100000+5];
 
 bool myCmp(Point A,Point B){
 	if(A.x!=B.x)return A.x<B.x;
@@ -39,7 +39,7 @@ Vector operator - (Vector A,Vector B){
 bool operator == (const Vector& A,const Vector& B){
 	return dcmp(A.x-B.x)==0&&dcmp(A.y-B.y)==0;
 }
-inline double Cross(Vector A,Vector B)  // 计算叉积
+inline double Cross(Vector A,Vector B)  
 {
 	return A.x * B.y - A.y * B.x;
 }
@@ -48,19 +48,17 @@ int ConvexHull()
 	sort(p,p+n,myCmp);
 	int m=0;
 	for(int i=0;i<n;i++){  
-	// 求下曲包，当该点在前进方向的右边时（使用叉积判断），删除前进的右端点，
-	// 循环，直到不在，并将该点加入，不在右边，则直接加入 
+
 		while(m>1&&dcmp(Cross(ch[m-1]-ch[m-2],p[i]-ch[m-2]))<=0)m--;
 		ch[m++]=p[i];
 	}
 	int k=m;
-	for(int i=n-2;i>=0;i--){  // 因 pn-1 已经遍历过，故从n-2开始 
-		// 求上凸包，最终形成合围，为完整凸包
+	for(int i=n-2;i>=0;i--){ 
 		while(m>k&&dcmp(Cross(ch[m-1]-ch[m-2],p[i]-ch[m-2]))<=0)m--;
 		ch[m++]=p[i]; 
 	}
-	if(n>1)m--;  // 因要形成凸包，故起始点重复，减1
-	return m;  //返回顶点个数
+	if(n>1)m--;  
+	return m; 
 }
 double Dis(Point A,Point B){
 	return sqrt((A.x-B.x)*(A.x-B.x)+(A.y-B.y)*(A.y-B.y));
@@ -74,14 +72,12 @@ int main(){
 		p[i]=Point(a,b);
 	}
 	m=ConvexHull();
-	
-	//下面是计算凸包周长的 
+
 	double ans=0.0; 
 	for(int i=0;i<m-1;i++)
 	  ans+=Dis(ch[i],ch[i+1]);
 	ans+=Dis(ch[m-1],ch[0]);
 	printf("%.2f ",ans);
-	// 下面是计算凸包面积的
 	 
     ans=0.0;
 	ch[m]=ch[0];
